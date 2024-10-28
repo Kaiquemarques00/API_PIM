d85113accc5890855108a01c8358462908aeb07e
Kaique marques <kaiquemarques581@outlook.com>
2024-10-28 16:59:55 -0300

    Corrigindo erro de acesso não autorizado ao banco 1

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const db = new pg.Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  port: process.env.PGPORT || 5432,
   ssl: {
     rejectUnauthorized: false,
   }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 //const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT ? Number(process.env.PORT) : 3333, () => {
+app.listen(process.env.PORT ? Number(process.env.PORT) : 5000, () => {
     console.log(`HTTP Server Running on port ${process.env.PORT}`);
   });
