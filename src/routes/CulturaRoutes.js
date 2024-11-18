4725ab53052a956521f7b72841d408416992c7bb
Kaique marques <kaiquemarques581@outlook.com>
2024-11-18 20:22:19 -0300

    Privando rotas das funcionalidade de plantio e cultura

--- src/routes/CulturaRoutes.js
+++ src/routes/CulturaRoutes.js
@@ -1,22 +1,25 @@
 import express from "express";
 
 import CulturaController from "../controllers/CulturaController.js";
+import Token from "../middlewares/CheckToken.js";
+import Role from "../middlewares/CheckRole.js";
 
 const culturaController = new CulturaController();
+const tokenMiddleware = new Token();
+const RoleMiddleware = new Role();
 
 const router = express.Router();
 
 // Rota de consulta de todos as culturas 
-router.get('/cultures', culturaController.buscaCulturas);
+router.get('/cultures', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.buscaCulturas);
 // Rota de consulta de uma cultura
-
-router.get('/culture/:id', culturaController.buscaCultura);
+router.get('/culture/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.buscaCultura);
 // Rota de criação de uma cultura
-router.post('/culture', culturaController.criaCultura);
+router.post('/culture', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.criaCultura);
 // Rota de alteração total de uma cultura
-router.put('/culture/:id', culturaController.alteraRegistro);
+router.put('/culture/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.alteraRegistro);
 // Rota de alteração parcial de uma cultura 
-router.patch('/culture/:id', culturaController.alteraCampo);
+router.patch('/culture/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.alteraCampo);
 // Rota de arquivação de uma cultura
-router.delete('/culture/:id', culturaController.deletaCultura);
+router.delete('/culture/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], culturaController.deletaCultura);
 export default router;
--- src/routes/PlantioRoutes.js
+++ src/routes/PlantioRoutes.js
@@ -1,22 +1,26 @@
 import express from "express";
 
 import PlantioController from "../controllers/PlantioController.js";
+import Token from "../middlewares/CheckToken.js";
+import Role from "../middlewares/CheckRole.js";
 
 const plantioController = new PlantioController();
+const tokenMiddleware = new Token();
+const RoleMiddleware = new Role();
 
 const router = express.Router();
 
 /* Rota de consulta de todos os plantios */
-router.get('/plantings', plantioController.buscaPlantios);
+router.get('/plantings', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.buscaPlantios);
 /* Rota de consulta de um plantio */
-router.get('/planting/:id', plantioController.buscaPlantio);
+router.get('/planting/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.buscaPlantio);
 /* Rota de criação de um plantio */
-router.post('/planting', plantioController.criaPlantio);
+router.post('/planting', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.criaPlantio);
 /* Rota de alteração total de um plantio */
-router.put('/planting/:id', plantioController.alteraRegistro);
+router.put('/planting/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.alteraRegistro);
 /* Rota de alteração parcial de um plantio */
-router.patch('/planting/:id', plantioController.alteraCampo);
+router.patch('/planting/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.alteraCampo);
 /* Rota de arquivação de um plantio */
-router.delete('/planting/:id', plantioController.deletaPlantio);
+router.delete('/planting/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], plantioController.deletaPlantio);
 
 export default router;
