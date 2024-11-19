3d243c9013af40c105e0219cea4fa4c947c26dc9
Kaique marques <kaiquemarques581@outlook.com>
2024-11-19 08:47:54 -0300

    Codificando funções de criar insumo a plantio, e listar

--- src/controllers/InsumosPlantioController.js
+++ src/controllers/InsumosPlantioController.js
@@ -2,7 +2,14 @@ import db from "../config/db.js";
 
 class InsumoPlantioController {
     async buscaInsumosPlantios(req, res) {
-        res.status(200).json("Rota de busca de insumos em plantios funcionando");
+        try {
+            const consultaInsumosPlantios = await db.query("SELECT * FROM insumos_plantios ORDER BY insumo_plantio_id ASC");
+            const insumosPlantios = consultaInsumosPlantios.rows;
+
+            res.status(200).json(insumosPlantios);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async buscaInsumoPlantio(req, res) {
@@ -10,7 +17,32 @@ class InsumoPlantioController {
     }
 
     async criaInsumoPlantio(req, res) {
-        res.status(200).json("Rota de criação de insumo em plantio funcionando");
+        const id = req.params.id;
+        const { insumo, qtd_insumo } = req.body;
+
+        if(!insumo) return res.status(422).json("Insumo deve ser preenchido");
+        if(typeof insumo != "string") return res.status(422).json("O campo INSUMO deve ser um texto");
+
+        if(!qtd_insumo) return res.status(422).json("Quantidade de insumo deve ser preenchida");
+        if(typeof qtd_insumo != "number") return res.status(422).json("O campo QUANTIDADE DE INSUMO deve ser um número");
+
+        try {
+            const consultaInsumo = await db.query("SELECT nome FROM insumos WHERE nome = $1", [insumo]);
+            if(consultaInsumo.rows.length === 0) return res.status(404).json("Insumo não encontrado");
+
+            const consultaPlantio = await db.query("SELECT plantio_id FROM plantios WHERE plantio_id = $1", [id]);
+            if (consultaPlantio.rows.length === 0) return res.status(404).json("Plantio não encontrado");
+
+            try {
+                await db.query("INSERT INTO insumos_plantios(plantio_id, insumo_nome, quantidade_utilizada) VALUES ($1, $2, $3)", [id, insumo, qtd_insumo]);
+            } catch (error) {
+                console.log(error);
+            }
+
+            res.json("Insumo direcionado a plantio com sucesso");
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async alteraRegistro(req, res) {
--- src/routes/InsumoPlantioRoutes.js
+++ src/routes/InsumoPlantioRoutes.js
@@ -11,7 +11,7 @@ router.get('/inputs_plantings', insumoPlantioController.buscaInsumosPlantios);
 /* Rota de consulta de um fornecedor */
 router.get('/input_planting/:id', insumoPlantioController.buscaInsumoPlantio);
 /* Rota de criação de um fornecedor */
-router.post('/input_planting', insumoPlantioController.criaInsumoPlantio);
+router.post('/input_planting/:id', insumoPlantioController.criaInsumoPlantio);
 /* Rota de alteração total de um fornecedor */
 router.put('/input_planting/:id', insumoPlantioController.alteraRegistro);
 /* Rota de alteração parcial de um fornecedor */
