b4d8ec5bfee9cb60c7a8bc66203658eabd334439
Kaique marques <kaiquemarques581@outlook.com>
2024-11-26 10:27:02 -0300

    Arrumando lógica da funcionalidade de pedidos

--- src/controllers/PedidoVendaController.js
+++ src/controllers/PedidoVendaController.js
@@ -32,37 +32,6 @@ class PedidoVendaController {
         }
     }
 
-    async buscaVendas(req, res) {
-        try {
-            const require = await db.query("SELECT * FROM produtos_vendidos ORDER BY produto_vendido_id ASC");
-      
-            const produtosVendidos = require.rows;
-  
-            if (produtosVendidos.length === 0) return res.status(404).json("Nenhum produto vendido encontrado");
-      
-            res.status(200).json(produtosVendidos);
-          } catch (error) {
-            console.log(error);
-            res.status(422).json("Erro ao acessar banco de dados");
-          }
-    }
-
-    async buscaVenda(req, res) {
-        const id = req.params.id;
-        
-        try {
-            const require = await db.query("SELECT * FROM produtos_vendidos WHERE produto_vendido_id = $1", [id]);
-
-            const produtoVendido = require.rows;
-
-            if(produtoVendido.length === 0) return res.status(404).json("Nenhum produto vendido encontrado");
-
-            res.status(200).json(produtoVendido)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async criaPedido(req, res) {
         const { status, usuario_id, observacoes } = req.body;
         const { cultura, quantidade, preco_unitario } = req.body;
@@ -76,7 +45,16 @@ class PedidoVendaController {
         const statusFormat = status.charAt(0).toUpperCase() + status.slice(1);
         if (statusFormat != "Pendente" && statusFormat != "Em andamento" && statusFormat != "Concluido") return res.status(422).json("Campo que faz referência ao status do pedido incorreto");
         
-        const dataAtual = new Date();
+        if(!cultura) return res.status(422).json("Nome da cultura deve ser preenchido");
+        if(typeof cultura !== "string") return res.status(422).json("O campo CULTURA deve ser um texto");
+
+        if(!quantidade) return res.status(422).json("Quantidade deve ser preenchido");
+        if(typeof quantidade !== "number") return res.status(422).json("O campo QUANTIDADE deve ser um número");
+
+        if(!preco_unitario) return res.status(422).json("Preço deve ser preenchido");
+        if(typeof preco_unitario !== "number") return res.status(422).json("O campo PREÇO UNITÁRIO deve ser um número");
+
+        const dataAtual = new Date()
         
         try {
             const checaUsuario = await db.query("SELECT * FROM usuarios WHERE usuario_id = $1", [usuario_id]);
@@ -84,43 +62,18 @@ class PedidoVendaController {
 
             if(usuarioResult.length === 0) return res.status(404).json("Usuário não existe");
 
-            const pedidoResult = await db.query(
-                `INSERT INTO pedidos(data_pedido, status, usuario_id, observacoes)
-                VALUES($1, $2, $3, $4) RETURNING *`,
-                [dataAtual, statusFormat, usuario_id, observacoes]
+            const checaCultura = await db.query("SELECT nome FROM culturas WHERE nome = $1", [cultura]);
+            const culturaResult = checaCultura.rows;
+
+            if(culturaResult.length === 0) return res.status(404).json("Cultura não existe");
+
+            await db.query(
+                `INSERT INTO pedidos(data_pedido, status, usuario_id, cultura_nome, quantidade, preco_unitario, observacoes)
+                VALUES($1, $2, $3, $4, $5, $6, $7)`,
+                [dataAtual, statusFormat, usuario_id, cultura, quantidade, preco_unitario, observacoes]
             );
-            const pedido = pedidoResult.rows[0]
-
-            if(pedido.status !== "Concluido") return res.status(201).json("Novo pedido cadastrado com sucesso");
-
-            if (pedido.status === "Concluido") {
-
-                if(!cultura) return res.status(422).json("Nome da cultura deve ser preenchido");
-                if(typeof cultura !== "string") return res.status(422).json("O campo CULTURA deve ser um texto");
-
-                const checaCultura = await db.query("SELECT nome FROM culturas WHERE nome = $1", [cultura]);
-                const culturaResult = checaCultura.rows;
-
-                if(culturaResult.length === 0) return res.status(404).json("Cultura não existe");
-
-                if(!quantidade) return res.status(422).json("Quantidade deve ser preenchido");
-                if(typeof quantidade !== "number") return res.status(422).json("O campo QUANTIDADE deve ser um número");
-
-                if(!preco_unitario) return res.status(422).json("Preço deve ser preenchido");
-                if(typeof preco_unitario !== "number") return res.status(422).json("O campo PREÇO UNITÁRIO deve ser um número");  
-
-                try {
-                    await db.query(
-                        `INSERT INTO produtos_vendidos(pedido_id, cultura_nome, quantidade, preco_unitario)
-                        VALUES($1, $2, $3, $4) RETURNING *`,
-                        [pedido.pedido_id, cultura, quantidade, preco_unitario]
-                    );
-                } catch (error) {
-                    console.log(error);
-                }
-
-                res.status(201).json("Novo produto vendido cadastrado com sucesso");
-            }
+            
+            res.status(201).json("Novo pedido cadastrado")
         } catch (error) {
             console.log(error);
         }
@@ -151,55 +104,43 @@ class PedidoVendaController {
             }
             dadosParaAtualizarPedido.usuario_id = usuario_id;
         }
+        if (cultura) {
+            try {
+                const checaCultura = await db.query("SELECT nome FROM culturas WHERE nome = $1", [cultura]);
+                const culturaResult = checaCultura.rows;
+    
+                if(culturaResult.length === 0) return res.status(404).json("Cultura não existe");
+
+                dadosParaAtualizarPedido.cultura_nome = cultura;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        if (quantidade) dadosParaAtualizarPedido.quantidade = quantidade;
+        if (preco_unitario) dadosParaAtualizarPedido.preco_unitario = preco_unitario;
         if (observacoes) dadosParaAtualizarPedido.observacoes = observacoes;
 
-        const dadosParaAtualizarVendas = {};
-        if (cultura) dadosParaAtualizarVendas.cultura_nome = cultura;
-        if (quantidade) dadosParaAtualizarVendas.quantidade = quantidade;
-        if (preco_unitario) dadosParaAtualizarVendas.preco_unitario = preco_unitario;
-    
-        if (
-            Object.keys(dadosParaAtualizarPedido).length === 0 &&
-            Object.keys(dadosParaAtualizarVendas).length === 0
-          )
-            return res.status(400).json("Nenhum campo do fornecedor para atualizar");
+        if (Object.keys(dadosParaAtualizarPedido).length === 0) return res.status(400).json("Nenhum campo do fornecedor para atualizar");
     
             const camposPedido = Object.keys(dadosParaAtualizarPedido);
             const valoresPedidos = Object.values(dadosParaAtualizarPedido);
             const setClauseOrders = camposPedido
               .map((campo, index) => `${campo} = $${index + 1}`)
               .join(", ");
-        
-            const camposVenda = Object.keys(dadosParaAtualizarVendas);
-            const valoresVendas = Object.values(dadosParaAtualizarVendas);
-            const setClauseSales = camposVenda
-              .map((campoA, indexA) => `${campoA} = $${indexA + 1}`)
-              .join(", ");
     
         try {
 
-            if (Object.keys(dadosParaAtualizarPedido).length > 0) {
-                const resultPedido = await db.query(
-                  `UPDATE pedidos SET ${setClauseOrders} WHERE pedido_id = $${
+            
+            const resultPedido = await db.query(
+                `UPDATE pedidos SET ${setClauseOrders} WHERE pedido_id = $${
                     camposPedido.length + 1
-                  } RETURNING *`,
-                  [...valoresPedidos, id]
-                );
-                if (resultPedido.rows.length === 0) return res.status(200).json("Pedido não existe")
-              }
-        
-              if (Object.keys(dadosParaAtualizarVendas).length > 0) {
-                const resultVendas = await db.query(
-                  `UPDATE produtos_vendidos SET ${setClauseSales} WHERE produto_vendido_id = $${
-                    camposVenda.length + 1
-                  } RETURNING *`,
-                  [...valoresVendas, id]
-                );
+                } RETURNING *`,
+                [...valoresPedidos, id]
+            );
 
-                if (resultVendas.rows.length === 0) return res.status(200).json("Produto vendido não existe")
-              }
+            if (resultPedido.rows.length === 0) return res.status(200).json("Pedido não existe")
     
-            res.status(200).json("Pedido/Venda alterada com sucesso");
+            res.status(200).json("Pedido alterado com sucesso");
         } catch (error) {
             console.log(error);
         }
@@ -222,25 +163,6 @@ class PedidoVendaController {
             console.log(error);
         }
     }
-
-    async deletaVenda(req, res) {
-        const id = req.params.id;    
-
-        try {
-            const require = await db.query("SELECT * FROM produtos_vendidos WHERE produto_vendido_id = $1", [id]);
-
-            const venda = require.rows;
-
-            if(venda.length === 0) return res.status(404).json("Nenhum pedido encontrado");
-
-            await db.query("DELETE FROM produtos_vendidos WHERE produto_vendido_id = $1", [id]);
-
-            res.status(200).json("Venda deletada com sucesso");
-        } catch (error) {
-            console.log(error);
-        }
-    }
-      
 }
 
 export default PedidoVendaController;
--- src/routes/PedidoRoutes.js
+++ src/routes/PedidoRoutes.js
@@ -14,17 +14,11 @@ const router = express.Router();
 router.get('/orders', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.buscaPedidos);
 // Rota de consulta de um pedido
 router.get('/order/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.buscaPedido);
-// Rota de consulta de todas as vendas
-router.get('/sales', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.buscaVendas);
-// Rota de consulta de uma venda
-router.get('/sale/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.buscaVenda);
 // Rota de criação de uma cultura
 router.post('/order', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.criaPedido);
 // Rota de alteração parcial de uma cultura 
 router.patch('/order/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.alteraCampo);
 // Rota de arquivação de uma cultura
 router.delete('/order/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.deletaPedido);
-// Rota deleta venda
-router.delete('/sale/:id', [tokenMiddleware.checkToken, RoleMiddleware.checkRole(['Administrador', 'Funcionario'])], pedidoController.deletaVenda);
 
 export default router;
